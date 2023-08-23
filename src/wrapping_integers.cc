9afaafe2ef11079a2f938f3738ca6c75fce09db1
chao <xiaochao200@126.com>
2023-08-23 12:23:39 +0000

    implement wrap and unwrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,16 +4,26 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return Wrap32 { ((zero_point.raw_value_ + uint32_t(n)) & 0xffffffff)};
+  // return Wrap32 {uint32_t(zero_point.raw_value_ + uint32_t(n))};
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint32_t abs_seqno = raw_value_ - zero_point.raw_value_;
+  uint32_t check32 = uint32_t(checkpoint);
+  uint64_t check64 = (checkpoint & 0xffffffff00000000);
+
+  if (check32 == abs_seqno) return check64 + abs_seqno;
+  if ((abs_seqno - check32) < (check32 - abs_seqno)) 
+  {
+    return abs_seqno > check32 ? 
+      check64 + abs_seqno : check64 + abs_seqno + 0x100000000;
+  }
+  else return abs_seqno > check32 ? 
+    (
+      (check64 == 0) ? 
+        abs_seqno : (check64 + abs_seqno - 0x100000000)
+    ) :
+    check64 + abs_seqno;
 }
