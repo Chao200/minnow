112e3406514500ca19c9f8a0c831bc70b9533f5d
chao <xiaochao200@126.com>
2023-08-24 02:33:36 +0000

    modify wrapping

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,26 +4,25 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  return Wrap32 { ((zero_point.raw_value_ + uint32_t(n)) & 0xffffffff)};
-  // return Wrap32 {uint32_t(zero_point.raw_value_ + uint32_t(n))};
+  return Wrap32 { (zero_point.raw_value_ + uint32_t(n)) };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  uint32_t abs_seqno = raw_value_ - zero_point.raw_value_;
+  uint32_t offset = raw_value_ - zero_point.raw_value_;
   uint32_t check32 = uint32_t(checkpoint);
   uint64_t check64 = (checkpoint & 0xffffffff00000000);
 
-  if (check32 == abs_seqno) return check64 + abs_seqno;
-  if ((abs_seqno - check32) < (check32 - abs_seqno)) 
+  if (check32 == offset) return check64 + offset;
+  if ((offset - check32) < (check32 - offset)) 
   {
-    return abs_seqno > check32 ? 
-      check64 + abs_seqno : check64 + abs_seqno + 0x100000000;
+    return offset > check32 ? 
+      check64 + offset : check64 + offset + 0x100000000;
   }
-  else return abs_seqno > check32 ? 
+  else return offset > check32 ? 
     (
       (check64 == 0) ? 
-        abs_seqno : (check64 + abs_seqno - 0x100000000)
+        offset : (check64 + offset - 0x100000000)
     ) :
-    check64 + abs_seqno;
+    check64 + offset;
 }
