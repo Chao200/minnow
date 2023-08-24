1c5ed2120d48a1bbdd1ba11e04891aaa3af03d03
chao <xiaochao200@126.com>
2023-08-24 05:15:16 +0000

    finish lab2

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -2,17 +2,41 @@
 
 using namespace std;
 
+TCPReceiver::TCPReceiver()
+: zero_point_(0), init_(false)
+{}
+
 void TCPReceiver::receive( TCPSenderMessage message, Reassembler& reassembler, Writer& inbound_stream )
 {
-  // Your code here.
-  (void)message;
-  (void)reassembler;
-  (void)inbound_stream;
+  if (message.SYN)
+  {
+    init_ = true;
+    zero_point_ = message.seqno;
+    // SYN 是不入 stream 的，所以 +1
+    message.seqno = message.seqno + 1;
+  }
+
+  if (init_)
+  {
+    // 将 seqno->absolute_seqno->stream_index
+    // 所以需要 -1
+    uint64_t first_index = message.seqno.unwrap(zero_point_, inbound_stream.bytes_pushed())-1;
+    reassembler.insert(first_index, message.payload, message.FIN, inbound_stream);
+  }
 }
 
 TCPReceiverMessage TCPReceiver::send( const Writer& inbound_stream ) const
-{
-  // Your code here.
-  (void)inbound_stream;
-  return {};
+{ 
+  uint16_t window_size = 
+    inbound_stream.available_capacity() > 0xffff ? 
+      0xffff : uint16_t(inbound_stream.available_capacity());
+
+  if (!init_) return TCPReceiverMessage {nullopt, window_size};
+
+  // stream_index->absolute_seqno->seq_no->ackno
+  Wrap32 ackno = Wrap32::wrap(inbound_stream.bytes_pushed(), zero_point_)+1;
+  
+  if (inbound_stream.is_closed()) ackno = ackno + 1;
+  
+  return TCPReceiverMessage {ackno, window_size};
 }
--- src/tcp_receiver.hh
+++ src/tcp_receiver.hh
@@ -6,7 +6,12 @@
 
 class TCPReceiver
 {
+private:
+  Wrap32 zero_point_;
+  bool init_;
+
 public:
+  TCPReceiver();
   /*
    * The TCPReceiver receives TCPSenderMessages, inserting their payload into the Reassembler
    * at the correct stream index.
