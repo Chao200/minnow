b1b8bdcc974ddef7f1e2220729468778ac29436c
chao <xiaochao200@126.com>
2023-08-26 09:08:56 +0000

    finish lab3

--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -3,11 +3,34 @@
 #include "byte_stream.hh"
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
-
+#include <list>
+ 
 class TCPSender
 {
-  Wrap32 isn_;
-  uint64_t initial_RTO_ms_;
+  // *********************************
+  // ************ 报文段相关 ************
+  Wrap32 isn_;   // 初始化 ISN
+  bool is_SYN_ = false;  // 是否为 SYN
+  bool is_FIN_ = false;  // 是否为 FIN
+  uint64_t abs_seqno_ = 0; // 记录 push 时，每一段的abs seqno，要用绝对序列号，因为相对序列号会溢出
+  std::deque<TCPSenderMessage> readied_segments_ = {};        // 记录所有准备好发送的段
+
+  // *********************************
+  // ************ 超时相关 ************
+  bool is_start_RTO_timer_ = false;   // 是否启动 RTO 计时器
+  uint64_t initial_RTO_ms_;   // 默认初始 RTO 时间，单位 ms
+  int RTO_ms_;  // 记录现在的 RTO，为什么 int 不可以变成 uint64_t 或者 size_t
+
+  // *********************************
+  // ************ 重传相关 ************
+  std::deque<TCPSenderMessage> outstanding_segments_ = {};    // 记录所有已发送未 ACK 的段
+  uint64_t outstanding_bytes_nums_ = 0;                       // 记录已发送未 ACK 的字节数
+  uint64_t consecutive_retransmissions_nums_ = 0;             // 连续重传次数
+  
+  // *********************************
+  // *********** 接收端信息 ************
+  TCPReceiverMessage rcv_msg_ = {}; // receiver 返回给 sender 的消息，即 ack 和 window_size
+  uint16_t window_size_ = 1;   // 记录接收方的窗口大小，初始为 1
 
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
